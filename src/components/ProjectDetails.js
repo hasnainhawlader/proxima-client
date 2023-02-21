a16c8f4b32adf8403c23229389d6c5b8357eaa92
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-02-21 22:35:05 +0600

    before auth

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { currencyFormator } from "../utils/currencyFormator";
 import { useProjectsContext } from "../hooks/useProjectsContext";
 import moment from "moment";
+import ProjectForm from "./ProjectForm";
 
 const ProjectDetails = ({ project }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isOverlayOpen, setOverlayOpen] = useState(false);
   const { dispatch } = useProjectsContext();
   const handledelete = async () => {
     const res = await fetch(
@@ -16,6 +19,14 @@ const ProjectDetails = ({ project }) => {
     if (res.ok) {
       dispatch({ type: "DELETE_PROJECT", payload: json });
     }
+  };
+  const handleUpdate = () => {
+    setIsModalOpen(true);
+    setOverlayOpen(true);
+  };
+  const handleOverlay = () => {
+    setIsModalOpen(false);
+    setOverlayOpen(false);
   };
   return (
     <div className="project bg-slate-800 p-5 rounded-xl shadow-xl border-slate-600 flex flex-col gap-5 w-[24rem]">
@@ -47,7 +58,10 @@ const ProjectDetails = ({ project }) => {
         </div>
       </div>
       <div className="bottom flex gap-5">
-        <button className="bg-sky-400 text-slate-900 py-2 px-5 rounded shadow-xl hover:bg-sky-50 duration-300">
+        <button
+          onClick={handleUpdate}
+          className="bg-sky-400 text-slate-900 py-2 px-5 rounded shadow-xl hover:bg-sky-50 duration-300"
+        >
           Update
         </button>
         <button
@@ -56,6 +70,29 @@ const ProjectDetails = ({ project }) => {
         >
           Delete
         </button>
+      </div>
+      {/*overlay */}
+
+      <div
+        onClick={handleOverlay}
+        className={`overlay fixed z-1 h-screen w-screen bg-slate-900/50 backdrop-blur-sm top-0 left-0 right-0 bottom-0 ${
+          isOverlayOpen ? "" : "hidden"
+        }`}
+      ></div>
+      {/*modal */}
+      <div
+        className={`update-modal overflow-y-auto w-[35rem]  h-screen fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-800 p-10 rounded-md shadow-lg z-2 ${
+          isModalOpen ? "" : "hidden"
+        }`}
+      >
+        <h2 className="text-3xl font-medium text-sky-400 mb-10">
+          Update project
+        </h2>
+        <ProjectForm
+          project={project}
+          setIsModalOpen={setIsModalOpen}
+          setOverlayOpen={setOverlayOpen}
+        />
       </div>
     </div>
   );
--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import { useProjectsContext } from "../hooks/useProjectsContext";
 
-const ProjectForm = () => {
-  const [title, setTitle] = useState("");
-  const [tech, setTech] = useState("");
-  const [budget, setBudget] = useState("");
-  const [duration, setDuration] = useState("");
-  const [manager, setManager] = useState("");
-  const [dev, setDev] = useState("");
+const ProjectForm = ({ project, setIsModalOpen, setOverlayOpen }) => {
+  const [title, setTitle] = useState(project ? project.title : "");
+  const [tech, setTech] = useState(project ? project.tech : "");
+  const [budget, setBudget] = useState(project ? project.budget : "");
+  const [duration, setDuration] = useState(project ? project.duration : "");
+  const [manager, setManager] = useState(project ? project.manager : "");
+  const [dev, setDev] = useState(project ? project.dev : "");
+
   const [error, setError] = useState(null);
   const [emptyFields, setEmptyFields] = useState([]);
 
@@ -18,67 +19,112 @@ const ProjectForm = () => {
     e.preventDefault();
     //data
     const projectObj = { title, tech, budget, duration, manager, dev };
-    const res = await fetch("http://localhost:4000/api/projects", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(projectObj),
-    });
-    const json = await res.json();
+    // if there is no project post request
 
-    // !res.ok set error
-    if (!res.ok) {
+    if (!project) {
+      const res = await fetch("http://localhost:4000/api/projects", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(projectObj),
+      });
+      const json = await res.json();
+
+      // !res.ok set error
       if (!res.ok) {
+        if (!res.ok) {
+          setError(json.error);
+          setEmptyFields(json.emptyFields);
+        }
+        console.log(emptyFields);
+
+        toast.error(`something went wrong`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "red",
+        });
+      }
+
+      // req.ok reset
+
+      if (res.ok) {
+        setTitle("");
+        setTech("");
+        setBudget("");
+        setDuration("");
+        setManager("");
+        setDev("");
+        setError("");
+        setEmptyFields([]);
+
+        // real time update
+
+        dispatch({ type: "CREATE_PROJECT", payload: json });
+
+        toast.success("well done", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      }
+      return;
+    }
+    // if there is a project
+    if (project) {
+      // send patch
+
+      const res = await fetch(
+        `http://localhost:4000/api/projects/${project._id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(projectObj),
+        }
+      );
+      const json = await res.json();
+      // !res.ok
+      if (!res.od) {
         setError(json.error);
         setEmptyFields(json.emptyFields);
       }
-      console.log(emptyFields);
+      // res ok
+      if (res.ok) {
+        setError(null);
+        setEmptyFields([]);
 
-      toast.error(`something went wrong`, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "red",
-      });
-    }
+        // dispatch
 
-    // req.ok reset
+        dispatch({ type: "UPDATE_PROJECT", payload: json });
 
-    if (res.ok) {
-      setTitle("");
-      setTech("");
-      setBudget("");
-      setDuration("");
-      setManager("");
-      setDev("");
-      setError("");
-      setEmptyFields([]);
+        // close overlay and modals
 
-      // real time update
-
-      dispatch({ type: "CREATE_PROJECT", payload: json });
-
-      toast.success("well done", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+        setIsModalOpen(false);
+        setOverlayOpen(false);
+      }
+      return;
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="projectForm flex flex-col gap-5">
-      <h2 className="text-3xl font-medium text-sky-400 mb-10">
+      <h2
+        className={`text-3xl font-medium text-sky-400 mb-10 ${
+          project ? "hidden" : ""
+        }`}
+      >
         Add a New Project
       </h2>
       <div className="form-control flex flex-col gap-2">
@@ -210,7 +256,7 @@ const ProjectForm = () => {
         type="submit"
         className="bg-sky-400 text-slate-900 py-3 rounded-lg hover:bg-sky-50 duration-50"
       >
-        Add project
+        {project ? "Confirm update" : "Add project"}
       </button>
     </form>
   );
